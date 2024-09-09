79407bb71a7922579153fe76d01675ddf4969a15
Sebastian Suarez <cbass428@gmail.com>
2024-09-09 14:51:39 -0400

    Implemented login and logout

--- store/models.py
+++ store/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 import datetime
 
+
 # Categories of Products
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
-    
+
     class Meta:
         verbose_name_plural = 'categories'
+
 
 # Customer
 class Customer(models.Model):
@@ -21,13 +23,15 @@ class Customer(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
-    
+
+
 # All products
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    description = models.CharField(max_length=250, default='',
+                                   blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
 
     # Sale info
@@ -36,6 +40,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
