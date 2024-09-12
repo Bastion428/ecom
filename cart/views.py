9012306237c871e91b6f2a24c0e8e5bd72b13c32
Sebastian Suarez <cbass428@gmail.com>
2024-09-12 14:32:37 -0400

    Shopping cart button now updates when products are added

--- cart/views.py
+++ cart/views.py
@@ -2,23 +2,24 @@ from django.shortcuts import render, get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 
 def cart_summary(request):
     return render(request, "cart_summary.html", {})
 
 
+@require_POST
 def cart_add(request):
     cart = Cart(request)
-    print("In the right view")
 
-    if request.POST.get('action') == 'post':
-        product_id = int(request.POST.get('product_id'))
-        product = get_object_or_404(Product, id=product_id)
+    product_id = int(request.POST.get('product_id'))
+    product = get_object_or_404(Product, id=product_id)
 
-        cart.add(product=product)
-        response = JsonResponse({'Product Name: ': product.name})
-        return response
+    cart.add(product=product)
+    cart_quantity = len(cart)
+    response = JsonResponse({'qty': cart_quantity})
+    return response
 
 
 def cart_delete(request):
