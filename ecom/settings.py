2a923c14eac1c7b9b27416b45bf51830e16dbf4c
Sebastian Suarez <cbass428@gmail.com>
2024-09-22 11:56:41 -0400

    Changed to using Postgres cloud database

--- ecom/settings.py
+++ ecom/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -84,8 +87,14 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('DB_TOP_SECRET_PASS'),
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '47568',
     }
 }
 
