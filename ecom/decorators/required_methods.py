de2f59ff16b54ba8e17295527b7eb98aa971b14c
Sebastian Suarez <cbass428@gmail.com>
2024-09-24 15:38:13 -0400

    Fixed decorator issue

--- ecom/decorators/required_methods.py
+++ ecom/decorators/required_methods.py
@@ -1,16 +1,18 @@
 from django.shortcuts import redirect
 from django.contrib import messages
-import functools
+from functools import wraps
 
 
-def required_methods_redirect(func, allowed_methods=['GET']):
-    """Redirects when method is not POST"""
-    @functools.wraps(func)
-    def wrapper(request, *args, **kwargs):
-        method_list = [allowed_methods] if isinstance(allowed_methods, str) else allowed_methods
-        if request.method in method_list:
-            return func(request, *args, **kwargs)
-        else:
-            messages.error(request, f'{request.method} is not an accepted method for the requested page. Access denied')
-            return redirect('home')
-    return wrapper
+def required_methods_redirect(allowed_methods=['GET']):
+    def decorator(func):
+        """Redirects when method is not POST"""
+        wraps(func)
+        def wrapper(request, *args, **kwargs):  # noqa: E306
+            method_list = [allowed_methods] if isinstance(allowed_methods, str) else allowed_methods
+            if request.method in method_list:
+                return func(request, *args, **kwargs)
+            else:
+                messages.error(request, f'{request.method} is not an accepted method for the requested page. Access denied')
+                return redirect('home')
+        return wrapper
+    return decorator
